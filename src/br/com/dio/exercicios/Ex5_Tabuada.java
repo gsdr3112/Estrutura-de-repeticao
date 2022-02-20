453e79386d6090b678b880ebc693dd83e51b9bfe
gsdr3112 <gsdr3112@hotmail.com>
2022-02-20 15:28:44 -0300

    Exercicio 5 loopFor

--- src/br/com/dio/exercicios/Ex5_Tabuada.java
+++ src/br/com/dio/exercicios/Ex5_Tabuada.java
@@ -4,6 +4,13 @@ import java.util.Scanner;
 
 public class Ex5_Tabuada {
     public static void main(String[] args) {
+        loopDoWhile();
+
+        loopFor();
+
+    }
+
+    public static void loopDoWhile() {
         Scanner scan = new Scanner(System.in);
         int numTabuada;
 
@@ -16,6 +23,22 @@ public class Ex5_Tabuada {
             System.out.println(numTabuada + " X " + cont + " = " + numTabuada * cont);
             cont++;
         } while (cont < 11);
+
+    }
+
+    public static void loopFor() {
+        Scanner scan = new Scanner(System.in);
+        int numTabuada;
+
+        System.out.println("Informe o número que deseja exibir a tabuada: ");
+        numTabuada = scan.nextInt();
+        int cont = 1;
+
+        System.out.println("Tabuada do " + numTabuada + ":");
+
+        for (int i = 1; i < 11; i++) {
+            System.out.println(numTabuada + " X " + i + " = " + numTabuada * i);
+        }
 
     }
 }
